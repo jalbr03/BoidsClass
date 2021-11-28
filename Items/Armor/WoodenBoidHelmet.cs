ab759ea8e322f7bbb745272bf961d21df923626e
jonathan <jalbr03>
2021-11-28 02:02:09 -0700

    started adding armor

---
+++ Items/Armor/WoodenBoidHelmet.cs
@@ -0,0 +1,69 @@
+﻿using Boids.Projectiles.Minions;
+using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.DataStructures;
+using Terraria.ModLoader;
+using Terraria.ID;
+using Terraria.GameContent.Creative;
+
+namespace Boids.Items.Armor
+{
+	// The AutoloadEquip attribute automatically attaches an equip texture to this item.
+	// Providing the EquipType.Head value here will result in TML expecting a X_Head.png file to be placed next to the item's main texture.
+	[AutoloadEquip(EquipType.Head)]
+	public class WoodenBoidHelmet : ModItem
+	{
+		public override void SetStaticDefaults() {
+			DisplayName.SetDefault("Wooden Boid Helmet");
+			Tooltip.SetDefault("There seams to be a distant chirping," 
+			                   + "\nGet the full set and see what comes.");
+
+			CreativeItemSacrificesCatalog.Instance.SacrificeCountNeededByItemId[Type] = 3;
+			ItemID.Sets.GamepadWholeScreenUseRange[Item.type] = true; // This lets the player target anywhere on the whole screen while using a controller
+			ItemID.Sets.LockOnIgnoresCollision[Item.type] = true;
+		}
+
+		public override void SetDefaults() {
+			Item.width = 18; // Width of the item
+			Item.height = 18; // Height of the item
+			Item.sellPrice(gold: 1); // How many coins the item is worth
+			Item.rare = ItemRarityID.Green; // The rarity of the item
+			Item.defense = 5; // The amount of defense the item will give when equipped
+		}
+
+		// IsArmorSet determines what armor pieces are needed for the setbonus to take effect
+		public override bool IsArmorSet(Item head, Item body, Item legs) {
+			return body.type == ModContent.ItemType<WoodenBoidBreastplate>() && legs.type == ModContent.ItemType<WoodenBoidLeggings>();
+		}
+
+		// UpdateArmorSet allows you to give set bonuses to the armor.
+		public override void UpdateArmorSet(Player player) {
+			player.setBonus = "Here come the Boids"; // This is the setbonus tooltip
+			Item.shoot = ModContent.ProjectileType<BoidMinion>();
+		}
+		
+		public override void ModifyShootStats(Player player, ref Vector2 position, ref Vector2 velocity, ref int type, ref int damage, ref float knockback) {
+			// Here you can change where the minion is spawned. Most vanilla minions spawn at the cursor position
+			position = Main.MouseWorld;
+		}
+		
+		public override bool Shoot(Player player, ProjectileSource_Item_WithAmmo source, Vector2 position, Vector2 velocity, int type, int damage, float knockback) {
+			// This is needed so the buff that keeps your minion alive and allows you to despawn it properly applies
+			player.AddBuff(Item.buffType, 2);
+	
+			// Minions have to be spawned manually, then have originalDamage assigned to the damage of the summon item
+			var projectile = Projectile.NewProjectileDirect(source, position, velocity, type, damage, knockback, Main.myPlayer);
+			projectile.originalDamage = Item.damage;
+	
+			// Since we spawned the projectile manually already, we do not need the game to spawn it for ourselves anymore, so return false
+			return false;
+		}
+
+		// Please see Content/ExampleRecipes.cs for a detailed explanation of recipe creation.
+		public override void AddRecipes() {
+			CreateRecipe().AddIngredient(ItemID.Wood, 20)
+				.AddTile(TileID.WorkBenches)
+				.Register();
+		}
+	}
+}
